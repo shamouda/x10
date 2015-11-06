70c7769676e5520fc7c2647d47b73533610e46b5
Sara S. Hamouda <sara.salem.hamouda@gmail.com>
2015-11-06 15:12:54 -0500

    merge with master

--- x10.runtime/src-cpp/x10aux/network.cc
+++ x10.runtime/src-cpp/x10aux/network.cc
@@ -40,7 +40,6 @@ using namespace x10aux;
 // caches to avoid repeatedly calling into x10rt for trivial things
 x10aux::place x10aux::here = -1;
 bool x10aux::x10rt_initialized = false;
-x10_int x10aux::num_local_cores = 1; // this will be set in template_main
 
 // keep a counter for the session.
 volatile x10_long x10aux::asyncs_sent = 0;
@@ -462,7 +461,7 @@ void x10aux::coll_handler(void *arg) {
     x10::xrx::FinishState* fs = (x10::xrx::FinishState*)arg;
     fs->notifyActivityTermination();
 }
-
+//used with ULFM, called only when a collective has failed due to a process failure
 void x10aux::failed_coll_handler(void *arg) {
     x10::xrx::FinishState* fs = (x10::xrx::FinishState*)arg;
     fs->notifyActivityTermination();
