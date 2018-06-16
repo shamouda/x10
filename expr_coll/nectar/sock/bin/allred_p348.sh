2da7cb11c43ac0165f3a74e981e0833e5ca83031
u5482878 <sara.salem@anu.edu.au>
2018-06-16 16:46:08 +1000

    coll_expr

---
+++ expr_coll/nectar/sock/bin/allred_p348.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+PROG_DIR=$X10_HOME/x10.tests/tests/MicroBenchmarks
+
+export X10_HOSTFILE=hosts_29.txt
+NODES=29
+P=348
+S=0
+X10_NPL=$((P+S))
+X10_NTHR=1
+
+export X10_MAX_THREADS=2250
+export X10_RESILIENT_FINISH_SMALL_ASYNC_SIZE=0
+
+for run in `seq 1 1`;
+do
+X10_NPLACES=$X10_NPL X10_RESILIENT_MODE=0  X10RT_X10_FORCE_COLLECTIVES=1 X10_NTHREADS=$X10_NTHR $PROG_DIR/BenchmarkAllreduce.sock &> ../out/allred.${P}x${X10_NTHR}.mode0gc.emu.run${run}.txt
+X10_NPLACES=$X10_NPL X10_RESILIENT_MODE=1  X10RT_X10_FORCE_COLLECTIVES=1 X10_NTHREADS=$X10_NTHR $PROG_DIR/BenchmarkAllreduce.sock &> ../out/allred.${P}x${X10_NTHR}.mode1.emu.run${run}.txt
+done
---
+++ expr_coll/raijin/bin/barr_p348.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#PBS -P v29
+#PBS -q normal
+#PBS -l walltime=00:04:00
+#PBS -l mem=220GB
+#PBS -l wd
+#PBS -l ncpus=352
+
+source ~/prof/x10_ulfm2.profile
+PROG_DIR=$X10_HOME/x10.tests/tests/MicroBenchmarks
+
+NODES=22
+P=348
+S=0
+
+X10_NPL=$((P+S))
+X10_NTHR=1
+
+MPIOPTS="-np $X10_NPL --bind-to core --map-by node --mca btl openib,vader,self"
+
+export X10_EXIT_BY_SIGKILL=1
+export X10_NUM_IMMEDIATE_THREADS=1
+export X10_MAX_THREADS=2250
+export X10_RESILIENT_FINISH_SMALL_ASYNC_SIZE=0
+
+for run in `seq 1 1`;
+do 
+X10_RESILIENT_MODE=0  X10_NTHREADS=$X10_NTHR mpirun $MPIOPTS $PROG_DIR/BenchmarkBarrier.mpi &> ../out/barr.${P}x${X10_NTHR}.mode0gc.mpi.run${run}.txt
+X10_RESILIENT_MODE=1  X10_NTHREADS=$X10_NTHR mpirun $MPIOPTS $PROG_DIR/BenchmarkBarrier.mpi &> ../out/barr.${P}x${X10_NTHR}.mode1.mpi.run${run}.txt
+
+X10_RESILIENT_MODE=0  X10RT_X10_FORCE_COLLECTIVES=1 X10_NTHREADS=$X10_NTHR mpirun $MPIOPTS $PROG_DIR/BenchmarkBarrier.mpi &> ../out/barr.${P}x${X10_NTHR}.mode0gc.emu.run${run}.txt
+X10_RESILIENT_MODE=1  X10RT_X10_FORCE_COLLECTIVES=1 X10_NTHREADS=$X10_NTHR mpirun $MPIOPTS $PROG_DIR/BenchmarkBarrier.mpi &> ../out/barr.${P}x${X10_NTHR}.mode1.emu.run${run}.txt
+ 
+done
